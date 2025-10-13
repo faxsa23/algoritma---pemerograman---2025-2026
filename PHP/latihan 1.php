556eab4893813b9cc440e519646de2accde486cf
Muhammad aufa <faxsa457@gmail.com>
2025-10-13 11:16:30 +0700

    Create latihan 1.php

---
+++ PHP/latihan 1.php
@@ -0,0 +1,11 @@
+<?php
+$nama = "muhammad aufa";
+$umur = 18;
+$tinggi =169;
+$aktif = true;
+
+echo "nama:$nama<br>";
+echo "umur:$umur<br>";
+echo "tinggi:$umur<br>";
+echo "status aktif:$aktif<br>";
+?>

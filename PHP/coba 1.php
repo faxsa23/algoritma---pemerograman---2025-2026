8f473aab6b161600f10f356611afed4dde1ec125
Muhammad aufa <faxsa457@gmail.com>
2025-10-13 09:45:37 +0700

    Create coba 1.php

---
+++ PHP/coba 1.php
@@ -0,0 +1,29 @@
+<html>
+<body bgcolor="white" text="black">
+
+<center><img src="c:\Users\SAMSUNG\Downloads\LOGO-UNIDA-BERTAUHID.png" width="100" height="100"></center>
+<br>
+
+<center>
+
+<?php
+$nama = "mahasiswa";
+$angka = 20;
+$a = 10;
+$b = 25;
+
+echo " universitas djuanda <br>";
+echo " fakulta ilmu komputer ";
+
+echo "<br>";
+echo "<br>";
+
+echo $nama;
+echo "<br>";
+echo $angka;
+echo "<br>";
+echo $a + $b;
+
+?>
+</body>
+</html>

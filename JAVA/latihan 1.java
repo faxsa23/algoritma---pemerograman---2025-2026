cc6bf64558e2d442858705ce1e741dc9ff51f005
Muhammad aufa <faxsa457@gmail.com>
2025-10-13 12:39:18 +0700

    Create latihan 1.java

---
+++ JAVA/latihan 1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.util.scanner;
+public class main {
+    public static void main (string[] args){
+        final double PI = 3.12159;
+        Scanner sc = new Scenner (system.in);
+        Syistem.out.print("masukan jari-jari:");
+        double r = sc.nextDouble();
+        double luas = PI*r*r;
+        System.out.println("Luas Lingkaran ="+luas);
+}
+}
